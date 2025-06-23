4dc9cab6c7e0f05d176e9b894636991db23a3190
Topgyal Gurung <topgyaltsering3@gmail.com>
2025-06-23 00:52:19 -0400

    changed index.js

--- web/routes/index.js
+++ web/routes/index.js
@@ -1,20 +1,18 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Nimble-ICE' })
-})
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Nimble-ICE" });
+});
 
 /* GET ice-flakes resource */
-router.get('/ice-flakes', function (req, res, next) {
-  res
-    .status(201)
-    .json({
-      resource: 'ice-flakes',
-      count: 1005,
-      shape: 'rectangle'
-    })
-})
+router.get("/ice-flakes", function (req, res, next) {
+  res.status(201).json({
+    resource: "ice-flakes",
+    count: 205,
+    shape: "circle",
+  });
+});
 
-module.exports = router
+module.exports = router;
