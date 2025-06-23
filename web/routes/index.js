c7ba9de8957090ac26a3a6099bd4e6a714fc59b0
Topgyal Gurung <topgyaltsering3@gmail.com>
2025-06-23 00:53:00 -0400

    Update index.js

--- web/routes/index.js
+++ web/routes/index.js
@@ -6,6 +6,7 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Nimble-ICE" });
 });
 
+
 /* GET ice-flakes resource */
 router.get("/ice-flakes", function (req, res, next) {
   res.status(201).json({
